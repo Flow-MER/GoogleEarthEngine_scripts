f88b9341290af83fac6b1df0f08032bf2f4fa6cb
Shane <you@example.com>
2025-03-27 02:28:31 +1100

    Landsat code cleanup

--- Landsat_Annual_NDVI_ANAEv3.js
+++ Landsat_Annual_NDVI_ANAEv3.js
@@ -3,97 +3,80 @@ var anaev3 = ee.FeatureCollection("projects/ee-litepc/assets/ANAEv3_gt1ha");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
 // #############################################################################
-// Flow-MER project (2025)  MDBA/CEWH
-// Shane Brooks shane@brooks.eco - 22/01/2025
+// BWS Vulnerabilities project (2022)  MDBA/CEWO
+// Shane Brooks shane@brooks.eco - 22/01/2023
 // This code uses AVHRR and MODIS NDVI products from Google's Earth Engine Library to
-// calculate the average NDVI per ANAE polygon per calendar year 
+// calculate the average NDVI per ANAE polygon per calendar year 1986-2022
+// The code would not run in one bite so was done in a couple of batches and outputs were appended
 
 
 // #############################################################################
-// polygons is the ANAEv3 susbset > 1 Ha 
+// polygons is the ANAEv3 susbset used for the BWS Vulnerabilities project.
+// It is the subset of ANAE redgum, blackbox and coolibah polygons > 1 Ha that intersect
+// the managed floodplain with some additonal polygons added that intersect MDBA waterbird areas and Ramar sites
+// e.g. to fix that Kerang Lakes is not on the MDBA "Managed Floodplain" layer
 
 //var polygons = anaev3
 var polygons = anaev3
     //.limit(50)   //limit polygons for testing
     ; 
-
+var mdb = ee.Geometry.Polygon(
+  [[[138.5, -37.6],
+  [152.5, -37.6],
+  [152.5, -24.5],
+  [138.5, -24.5]]], null, false);
 
 // #############################################################################
-// Prep the image collections. 
+// Prep the image collections.
 // the full sequence of 1987-2022 requires two NDVI data sources, both are 250m x 16day
 // so we convert to annual averages
 
 
 
-var Landsat_NDVI = ee.ImageCollection('LANDSAT/COMPOSITES/C02/T1_L2_32DAY_NDVI');
+
+var short_source = 'Landsat';
+var image_collection_name = 'LANDSAT/COMPOSITES/C02/T1_L2_32DAY_NDVI';
+var start_year = 1987;  //1986 is partial only
+var end_year = 2024;
+
+//var short_source = 'MODIS'
+//var image_collection_name = 'MODIS/061/MOD13Q1';
+//var start_year = 2001;
+//var end_year = 2024;
+
+var export_filename = 'NDVI_' + start_year.toString() + '-' + end_year.toString() + '_ANAEv3_annual_' + short_source
 
 
-//Now if we want to mask every Image in an Image Collection we use the same code but generalize it and put it into the function to map over:
+var ndvi_collection = ee.ImageCollection(image_collection_name);
 
-
-//Mask each image in the collection to positive NDVI to exclude water
-var Landsat_NDVI_masked = Landsat_NDVI.map(function(image){
-  return image.mask(image.select("NDVI").gt(0))
+//Function to mask each image in the collection to positive NDVI to exclude water
+var ndvi_masked = ndvi_collection.map(function (image) {
+  return image.mask(image.select("NDVI").gt(0)).clip(mdb).select("NDVI")
 })
 
-
-
-
-
-
-var Landsat_years = ee.List.sequence(1986, 2024, 1).map(function(year){
-  var start = ee.Date.fromYMD(year, 1, 1);
-  var end = start.advance(1, 'year');
-  var ndviMeanYear = Landsat_NDVI_masked //Landsat_NDVI.select('NDVI')
-                              .filterDate(start, end).mean();//.multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
-});
-var NDVI_1986_2024_landsat = ee.ImageCollection(Landsat_years); 
-
-
-
-
-
-
-var AVHRR_NDVI = ee.ImageCollection("NOAA/CDR/AVHRR/NDVI/V5");
 //make a list of years and filter the 16 day NDVI into annual averages
-var AVHRR_years = ee.List.sequence(1986, 2000, 1).map(function(year){
+var annual_ndvi = ee.List.sequence(start_year, end_year, 1).map(function (year) {
   var start = ee.Date.fromYMD(year, 1, 1);
   var end = start.advance(1, 'year');
-  var ndviMeanYear = AVHRR_NDVI.select('NDVI')
-                              .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
+  return ndvi_masked
+    .filterDate(start, end)
+    .mean()//.multiply(0.0001)   //landsat data set is already -1 to 1
+    .set('year', start.format("YYYY"));
 });
 
-var NDVI_1986_2000 = ee.ImageCollection(AVHRR_years);  
-//print ('NDVI_1986_2000', NDVI_1986_1999);
-
-var modisNDVI = ee.ImageCollection('MODIS/061/MOD13Q1');
-//make a list of years and filter the 16 day NDVI into annual averages
-var modis_years = ee.List.sequence(2001, 2024, 1).map(function(year){
-  var start = ee.Date.fromYMD(year, 1, 1);
-  var end = start.advance(1, 'year');
-  var ndviMeanYear = modisNDVI.select('NDVI')
-                              .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
-});
-var NDVI_2001_2024 = ee.ImageCollection(modis_years);
-//print ('NDVI_2000_2022', NDVI_2000_2022);
-
-
-// Can merge the two sequences but to speed up processing each set was run separately
-// first attempt at 37 years together timed out after 12 hours
-//var allNDVI = NDVI_1986_1999.merge(NDVI_2000_2022);
-
-// #############################################################################
-// *** Toggle these to process each batch
-// *** DONT FORGET TO RENAME the exported filename -see 'description' below
-//
-// running times 30mins-10h - took a few attempts to complete.
-//
-var rasters_to_analyse = NDVI_1986_2024_landsat;
-
+var rasters_to_analyse = ee.ImageCollection(annual_ndvi);
+print(export_filename);
 print ('rasters_to_analyse', rasters_to_analyse);
+var ndvi_img = rasters_to_analyse.first();
+var ndviVis = {
+  min: 0.0,
+  max: 1.0,
+  palette: [
+    'ffffff', 'ce7e45', 'fcd163', 'c6ca02', '22cc04', '99b718', '207401',
+    '012e01'
+  ],
+};
+Map.addLayer(ndvi_img, ndviVis, 'NDVI');
 
 
 // Function to be mapped over the NDVI image collection that calculates
@@ -133,7 +116,7 @@ var export_zonal_stats = zonal_stats.select([".*"], null, false);
 //** CHANGE 'description' to identify which NDVI image collection is run
 Export.table.toDrive({
   collection: export_zonal_stats,
-  description: 'NDVI_1986_2024_landsat_annual_anaev3',
+  description: export_filename,
   fileFormat: 'CSV',
   selectors: ['UID', 'year','NDVI']
 });
