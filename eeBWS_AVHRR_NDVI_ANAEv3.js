a8b48790d23797eba82f425f6c6b4b37f85e924e
Shane <you@example.com>
2025-03-27 02:15:36 +1100

    code cleanup

--- eeBWS_AVHRR_NDVI_ANAEv3.js
+++ eeBWS_AVHRR_NDVI_ANAEv3.js
@@ -2,77 +2,87 @@
 var anaev3 = ee.FeatureCollection("projects/ee-litepc/assets/ANAEv3_gt1ha");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
+
 // #############################################################################
 // BWS Vulnerabilities project (2022)  MDBA/CEWO
-// Shane Brooks shane@brooks.eco - 22/01/2023
+// rejigged for Flow-MER (2024)
+// Shane Brooks shane@brooks.eco
 // This code uses AVHRR and MODIS NDVI products from Google's Earth Engine Library to
-// calculate the average NDVI per ANAE polygon per calendar year 1986-2022
-// The code would not run in one bite so was done in a couple of batches and outputs were appended
+// calculate the average NDVI per ANAE polygon per calendar year 1986-2024
+// The code would not run in one bite so was done in two batches and outputs were harmonised and appended
 
 
 // #############################################################################
-// polygons is the ANAEv3 susbset > 1 Ha 
+// BWS Vulnerabilities project (2022)  MDBA/CEWO
+// rejigged for Flow-MER (2024)
+// Shane Brooks shane@brooks.eco
+// This code uses AVHRR and MODIS NDVI products from Google's Earth Engine Library to
+// calculate the average NDVI per ANAE polygon per calendar year 1986-2024
+// The code would not run in one bite so was done in two batches and outputs were harmonised and appended
 
+
+// #############################################################################
+// polygons is the ANAEv3 susbset > 1 Ha as small polys do not reliably align to pixel grids and fail to rasterise
+//analysis is done with scale of 30m
+
+//var polygons = anaev3
 var polygons = anaev3
     //.limit(10)   //limit polygons for testing
     ; 
-
+var mdb = ee.Geometry.Polygon(
+  [[[138.5, -37.6],
+  [152.5, -37.6],
+  [152.5, -24.5],
+  [138.5, -24.5]]], null, false);
 
 // #############################################################################
-// Prep the image collections. 
-// the full sequence of 1987-2022 requires two NDVI data sources, both are 250m x 16day
+// Prep the image collections.
+// the full sequence of 1987-2024 requires two NDVI data sources, both are 250m x 16day
 // so we convert to annual averages
 
-var AVHRR_NDVI = ee.ImageCollection("NOAA/CDR/AVHRR/NDVI/V5");
+var short_source = 'AVHRR';
+var image_collection_name = 'NOAA/CDR/AVHRR/NDVI/V5';
+var start_year = 1986;
+var end_year = 2000;
+
+// var short_source = 'MODIS'
+// var image_collection_name = 'MODIS/061/MOD13Q1';
+// var start_year = 2001;
+// var end_year = 2024;
+
+var export_filename = 'NDVI_' + start_year.toString() + '-' + end_year.toString() + '_ANAEv3_annual_' + short_source
+
+
+var ndvi_collection = ee.ImageCollection(image_collection_name);
 
 //Function to mask each image in the collection to positive NDVI to exclude water
-var AVHRR_NDVI_masked = AVHRR_NDVI.map(function(image){
-  return image.mask(image.select("NDVI").gt(0))
+var ndvi_masked = ndvi_collection.map(function (image) {
+  return image.mask(image.select("NDVI").gt(0)).clip(mdb).select("NDVI")
 })
 
 //make a list of years and filter the 16 day NDVI into annual averages
-var AVHRR_years = ee.List.sequence(1986, 2000, 1).map(function(year){
+var annual_ndvi = ee.List.sequence(start_year, end_year, 1).map(function (year) {
   var start = ee.Date.fromYMD(year, 1, 1);
   var end = start.advance(1, 'year');
-  var ndviMeanYear = AVHRR_NDVI_masked
-                              .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
+  return ndvi_masked
+    .filterDate(start, end)
+    .mean().multiply(0.0001)
+    .set('year', start.format("YYYY"));
 });
 
-var NDVI_1986_2000 = ee.ImageCollection(AVHRR_years);  
-//print ('NDVI_1986_2000', NDVI_1986_1999);
-
-var modisNDVI = ee.ImageCollection('MODIS/061/MOD13Q1');
-//Function to mask each image in the collection to positive NDVI to exclude water
-var modisNDVI_masked = modisNDVI.map(function(image){
-  return image.mask(image.select("NDVI").gt(0))
-})
-//make a list of years and filter the 16 day NDVI into annual averages
-var modis_years = ee.List.sequence(2001, 2024, 1).map(function(year){
-  var start = ee.Date.fromYMD(year, 1, 1);
-  var end = start.advance(1, 'year');
-  var ndviMeanYear = modisNDVI_masked
-                              .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
-});
-var NDVI_2001_2024 = ee.ImageCollection(modis_years);
-//print ('NDVI_2000_2022', NDVI_2000_2022);
-
-
-// Can merge the two sequences but to speed up processing each set was run separately
-// first attempt at 37 years together timed out after 12 hours
-//var allNDVI = NDVI_1986_1999.merge(NDVI_2000_2022);
-
-// #############################################################################
-// *** Toggle these to process each batch
-// *** DONT FORGET TO RENAME the exported filename -see 'description' below
-//
-// running times 30mins-10h - took a few attempts to complete.
-//
-var rasters_to_analyse = NDVI_1986_2000;
-
+var rasters_to_analyse = ee.ImageCollection(annual_ndvi);
+print(export_filename);
 print ('rasters_to_analyse', rasters_to_analyse);
-
+var ndvi_img = rasters_to_analyse.first();
+var ndviVis = {
+  min: 0.0,
+  max: 1.0,
+  palette: [
+    'ffffff', 'ce7e45', 'fcd163', 'c6ca02', '22cc04', '99b718', '207401',
+    '012e01'
+  ],
+};
+Map.addLayer(ndvi_img, ndviVis, 'NDVI');
 
 // Function to be mapped over the NDVI image collection that calculates
 // mean NDVI per image for all polygons using the 'reduceRegions' method.
@@ -111,7 +121,7 @@ var export_zonal_stats = zonal_stats.select([".*"], null, false);
 //** CHANGE 'description' to identify which NDVI image collection is run
 Export.table.toDrive({
   collection: export_zonal_stats,
-  description: 'NDVI_1986_2000_annual_ANAEv3_AVHRR',
+  description: export_filename,
   fileFormat: 'CSV',
   selectors: ['UID', 'year','NDVI']
 });
--- eeBWS_MODIS_NDVI_ANAEv3.js
+++ eeBWS_MODIS_NDVI_ANAEv3.js
@@ -4,195 +4,75 @@ var anaev3 = ee.FeatureCollection("projects/ee-litepc/assets/ANAEv3_gt1ha");
 
 // #############################################################################
 // BWS Vulnerabilities project (2022)  MDBA/CEWO
-// Shane Brooks shane@brooks.eco - 22/01/2023
+// rejigged for Flow-MER (2024)
+// Shane Brooks shane@brooks.eco
 // This code uses AVHRR and MODIS NDVI products from Google's Earth Engine Library to
-// calculate the average NDVI per ANAE polygon per calendar year 1986-2022
-// The code would not run in one bite so was done in a couple of batches and outputs were appended
+// calculate the average NDVI per ANAE polygon per calendar year 1986-2024
+// The code would not run in one bite so was done in two batches and outputs were harmonised and appended
 
 
 // #############################################################################
-// polygons is the ANAEv3 susbset  > 1 Ha
-
-
-// #############################################################################
-// BWS Vulnerabilities project (2022)  MDBA/CEWO
-// Shane Brooks shane@brooks.eco - 22/01/2023
-// This code uses AVHRR and MODIS NDVI products from Google's Earth Engine Library to
-// calculate the average NDVI per ANAE polygon per calendar year 1986-2022
-// The code would not run in one bite so was done in a couple of batches and outputs were appended
-
-
-// #############################################################################
-// polygons is the ANAEv3 susbset used for the BWS Vulnerabilities project.
-// It is the subset of ANAE redgum, blackbox and coolibah polygons > 1 Ha that intersect
-// the managed floodplain with some additonal polygons added that intersect MDBA waterbird areas and Ramar sites
-// e.g. to fix that Kerang Lakes is not on the MDBA "Managed Floodplain" layer
+// polygons is the ANAEv3 susbset > 1 Ha as small polys do not reliably align to pixel grids and fail to rasterise
+//analysis is done with scale of 30m
 
 //var polygons = anaev3
 var polygons = anaev3
   //.limit(10)   //limit polygons for testing
   ;
-
+var mdb = ee.Geometry.Polygon(
+  [[[138.5, -37.6],
+  [152.5, -37.6],
+  [152.5, -24.5],
+  [138.5, -24.5]]], null, false);
 
 // #############################################################################
-// Prep the image collections. 
-// the full sequence of 1987-2022 requires two NDVI data sources, both are 250m x 16day
+// Prep the image collections.
+// the full sequence of 1987-2024 requires two NDVI data sources, both are 250m x 16day
 // so we convert to annual averages
 
-var AVHRR_NDVI = ee.ImageCollection("NOAA/CDR/AVHRR/NDVI/V5");
+// var short_source = 'AVHRR'
+// var image_collection_name = 'NOAA/CDR/AVHRR/NDVI/V5';
+// var start_year = 1986;
+// var end_year = 2000;
+
+var short_source = 'MODIS'
+var image_collection_name = 'MODIS/061/MOD13Q1';
+var start_year = 2001;
+var end_year = 2024;
+
+var export_filename = 'NDVI_' + start_year.toString() + '-' + end_year.toString() + '_ANAEv3_annual_' + short_source
+
+
+var ndvi_collection = ee.ImageCollection(image_collection_name);
 
 //Function to mask each image in the collection to positive NDVI to exclude water
-var AVHRR_NDVI_masked = AVHRR_NDVI.map(function (image) {
-  return image.mask(image.select("NDVI").gt(0))
+var ndvi_masked = ndvi_collection.map(function (image) {
+  return image.mask(image.select("NDVI").gt(0)).clip(mdb).select("NDVI")
 })
 
 //make a list of years and filter the 16 day NDVI into annual averages
-var AVHRR_years = ee.List.sequence(1986, 2000, 1).map(function (year) {
+var annual_ndvi = ee.List.sequence(start_year, end_year, 1).map(function (year) {
   var start = ee.Date.fromYMD(year, 1, 1);
   var end = start.advance(1, 'year');
-  var ndviMeanYear = AVHRR_NDVI_masked
-    .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
+  return ndvi_masked
+    .filterDate(start, end)
+    .mean().multiply(0.0001)
+    .set('year', start.format("YYYY"));
 });
 
-var NDVI_1986_2000 = ee.ImageCollection(AVHRR_years);
-//print ('NDVI_1986_2000', NDVI_1986_1999);
-
-var modisNDVI = ee.ImageCollection('MODIS/061/MOD13Q1');
-//Function to mask each image in the collection to positive NDVI to exclude water
-var modisNDVI_masked = modisNDVI.map(function (image) {
-  return image.mask(image.select("NDVI").gt(0))
-})
-//make a list of years and filter the 16 day NDVI into annual averages
-var modis_years = ee.List.sequence(2001, 2024, 1).map(function (year) {
-  var start = ee.Date.fromYMD(year, 1, 1);
-  var end = start.advance(1, 'year');
-  var ndviMeanYear = modisNDVI_masked
-    .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
-});
-var NDVI_2001_2024 = ee.ImageCollection(modis_years);
-//print ('NDVI_2000_2022', NDVI_2000_2022);
-
-
-// Can merge the two sequences but to speed up processing each set was run separately
-// first attempt at 37 years together timed out after 12 hours
-//var allNDVI = NDVI_1986_1999.merge(NDVI_2000_2022);
-
-// #############################################################################
-// *** Toggle these to process each batch
-// *** DONT FORGET TO RENAME the exported filename -see 'description' below
-//
-// running times 30mins-10h - took a few attempts to complete.
-//
-var rasters_to_analyse = NDVI_1986_2000;
-
-print('rasters_to_analyse', rasters_to_analyse);
-
-
-// Function to be mapped over the NDVI image collection that calculates
-// mean NDVI per image for all polygons using the 'reduceRegions' method.
-// analogous to GIS zonal statistics.
-
-var reduceRegions = function (image) {
-  var ndvi = image.reduceRegions({
-    collection: polygons,
-    reducer: ee.Reducer.mean(),
-    scale: 30,  //modis is 250m but dont want to loose small/narrow polys in the calc
-    tileScale: 4
-  });
-  return ndvi
-    // ...remove any features that have a null value for any property.
-    .filter(ee.Filter.notNull(['mean']))
-    // ...map over the featureCollection to select and rename properties of interest.
-    .map(function (feature) {
-      return feature
-        .select(['mean', 'UID'], ['NDVI', 'UID'])
-        .set({ 'year': image.get('year') });
-    });
-};
-
-
-// Apply the above defined function to all images. 
-var zonal_stats = rasters_to_analyse.map(reduceRegions)
-  .flatten();
-
-
-// Strip out the geometry for export
-var export_zonal_stats = zonal_stats.select([".*"], null, false);
-
-
-
-// Export the table of results to GoogleDrive as a CSV file
-//** CHANGE 'description' to identify which NDVI image collection is run
-Export.table.toDrive({
-  collection: export_zonal_stats,
-  description: 'NDVI_1986_2000_annual_ANAEv3_AVHRR',
-  fileFormat: 'CSV',
-  selectors: ['UID', 'year', 'NDVI']
-});
-
-// Note the various output tables were concatenated in python into a single 1986-2022 data file
-var polygons = anaev3
-    //.limit(10)   //limit polygons for testing
-    ; 
-
-
-// #############################################################################
-// Prep the image collections. 
-// the full sequence of 1987-2022 requires two NDVI data sources, both are 250m x 16day
-// so we convert to annual averages
-
-var AVHRR_NDVI = ee.ImageCollection("NOAA/CDR/AVHRR/NDVI/V5");
-
-//Function to mask each image in the collection to positive NDVI to exclude water
-var AVHRR_NDVI_masked = AVHRR_NDVI.map(function(image){
-  return image.mask(image.select("NDVI").gt(0))
-})
-
-//make a list of years and filter the 16 day NDVI into annual averages
-var AVHRR_years = ee.List.sequence(1986, 2000, 1).map(function(year){
-  var start = ee.Date.fromYMD(year, 1, 1);
-  var end = start.advance(1, 'year');
-  var ndviMeanYear = AVHRR_NDVI_masked
-                              .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
-});
-
-var NDVI_1986_2000 = ee.ImageCollection(AVHRR_years);  
-//print ('NDVI_1986_2000', NDVI_1986_1999);
-
-var modisNDVI = ee.ImageCollection('MODIS/061/MOD13Q1');
-//Function to mask each image in the collection to positive NDVI to exclude water
-var modisNDVI_masked = modisNDVI.map(function(image){
-  return image.mask(image.select("NDVI").gt(0))
-})
-//make a list of years and filter the 16 day NDVI into annual averages
-var modis_years = ee.List.sequence(2001, 2024, 1).map(function(year){
-  var start = ee.Date.fromYMD(year, 1, 1);
-  var end = start.advance(1, 'year');
-  var ndviMeanYear = modisNDVI_masked
-                              .filterDate(start, end).mean().multiply(0.0001);
-  return ndviMeanYear.set('year', start.format("YYYY"));
-});
-var NDVI_2001_2024 = ee.ImageCollection(modis_years);
-//print ('NDVI_2000_2022', NDVI_2000_2022);
-
-
-// Can merge the two sequences but to speed up processing each set was run separately
-// first attempt at 37 years together timed out after 12 hours
-//var allNDVI = NDVI_1986_1999.merge(NDVI_2000_2022);
-
-// #############################################################################
-// *** Toggle these to process each batch
-// *** DONT FORGET TO RENAME the exported filename -see 'description' below
-//
-// running times 30mins-10h - took a few attempts to complete.
-//
-var rasters_to_analyse = NDVI_2001_2024;
-
+var rasters_to_analyse = ee.ImageCollection(annual_ndvi);
+print(export_filename);
 print ('rasters_to_analyse', rasters_to_analyse);
-
+var ndvi_img = rasters_to_analyse.first();
+var ndviVis = {
+  min: 0.0,
+  max: 1.0,
+  palette: [
+    'ffffff', 'ce7e45', 'fcd163', 'c6ca02', '22cc04', '99b718', '207401',
+    '012e01'
+  ],
+};
+Map.addLayer(ndvi_img, ndviVis, 'NDVI');
 
 // Function to be mapped over the NDVI image collection that calculates
 // mean NDVI per image for all polygons using the 'reduceRegions' method.
@@ -231,7 +111,7 @@ var export_zonal_stats = zonal_stats.select([".*"], null, false);
 //** CHANGE 'description' to identify which NDVI image collection is run
 Export.table.toDrive({
   collection: export_zonal_stats,
-  description: 'NDVI_2001_2024_annual_ANAEv3_MODIS',
+  description: export_filename,
   fileFormat: 'CSV',
   selectors: ['UID', 'year','NDVI']
 });
